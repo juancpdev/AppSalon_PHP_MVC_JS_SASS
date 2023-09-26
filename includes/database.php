637e64eab1685fc252bdc3eb3766a50b48c4f4c2
Juan Cruz <juan_pineda_12@hotmail.es>
2023-09-26 16:38:07 +0000

    deployt

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'ROOT', 'appsalon_mvc');
+$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
 $db->set_charset("utf8");
 
 if (!$db) {
